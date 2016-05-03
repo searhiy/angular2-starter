de615725c0fc68dd6555f7851366e7eb451272ed
Serhii Kartashov <kartashovsearhiy@gmail.com>
2016-05-03 16:16:21 +0300

    Updated to RC.0 version

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,6 @@
-import {Component} from 'angular2/core';
-
+import {Component} from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 Starter App</h1>'
+    template: '<h1>My First Angular 2 App</h1>'
 })
-export class AppComponent{}
+export class AppComponent { }
